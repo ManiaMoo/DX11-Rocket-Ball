1f73406c5d975a4f19d298097ad0f7759e742d83
Unknown <crazymonks117@gmail.com>
2017-12-26 21:14:51 +0000

    Rendering multiple different objects!

--- Application.h
+++ Application.h
@@ -25,7 +25,7 @@ struct ConstantBuffer
 class Application
 {
 private:
-	static const int        _cubenumber = 2;
+	static const int        _cubenumber = 5;
 	HINSTANCE               _hInst;
 	HWND                    _hWnd;
 	D3D_DRIVER_TYPE         _driverType;
@@ -38,15 +38,19 @@ private:
 	ID3D11PixelShader*      _pPixelShader;
 	ID3D11InputLayout*      _pVertexLayout;
 	ID3D11RasterizerState*	_wireFrame;
-	ID3D11Buffer*           _pVertexBuffer;
-	ID3D11Buffer*           _pIndexBuffer;
+	ID3D11Buffer*           _pVertexBufferCube;
+	ID3D11Buffer*           _pVertexBufferPyramid;
+	ID3D11Buffer*           _pVertexBufferPlane;
+	ID3D11Buffer*           _pIndexBufferCube;
+	ID3D11Buffer*           _pIndexBufferPyramid;
+	ID3D11Buffer*           _pIndexBufferPlane;
 	ID3D11Buffer*           _pConstantBuffer;
 	ID3D11DepthStencilView* _depthStencilView;
 	ID3D11Texture2D*        _depthStencilBuffer;
-	int						_isWireframe;
-	XMFLOAT4X4              _world[5];
+	XMFLOAT4X4              _world[_cubenumber];
 	XMFLOAT4X4              _view;
 	XMFLOAT4X4              _projection;
+	int						_isWireframe;
 
 private:
 	HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
@@ -54,8 +58,12 @@ private:
 	void Cleanup();
 	HRESULT CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut);
 	HRESULT InitShadersAndInputLayout();
-	HRESULT InitVertexBuffer();
-	HRESULT InitIndexBuffer();
+	HRESULT InitVertexBufferCube();
+	HRESULT InitVertexBufferPyramid();
+	HRESULT InitVertexBufferPlane();
+	HRESULT InitIndexBufferCube();
+	HRESULT InitIndexBufferPyramid();
+	HRESULT InitIndexBufferPlane();
 
 	UINT _WindowHeight;
 	UINT _WindowWidth;
